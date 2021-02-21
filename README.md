da4963075ef1586ba8df2ada9ec0d1f442371323
Abdurahman Ahmed <71194018+abdurahman@users.noreply.github.com>
2021-02-20 19:28:45 -0500

    added .gitignore

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Lab 4 - User Database
+User database using MongoDB, NodeJS, ExpressJS and Mongoose
---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const usersRouter = require('./routes/UserRoutes');
+
+const app = express();
